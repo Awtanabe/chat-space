27e492c15ecf8e9b01cd17e7225c10bbcb0052ed
Awtanabe <panser098@gmail.com>
2018-04-20 17:08:24 +0900

    ajaxの発火

---
+++ app/assets/javascripts/message_update.js
@@ -0,0 +1,15 @@
+$(function(){
+  $(document).on('submit', '#new_message', function(e){
+    e.preventDefault();
+    var formData = new FormData(this);
+    var url = $(this).attr('action');
+    $.ajax({
+      url: url,
+      type: "POST",
+      data: formData,
+      datatype: 'json',
+      processData: false,
+      contentType: false
+    })
+  })
+})
