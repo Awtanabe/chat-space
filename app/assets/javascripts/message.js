74264dbef78a9cb094903992ccdecb61612b78db
Awtanabe <panser098@gmail.com>
2018-04-20 17:51:18 +0900

    自動更新機能追加

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,4 +1,15 @@
 $(function() {
+
+    setInterval(function() {
+        $.ajax({
+                url: location.href.json,
+            })
+            .done(function(data) {})
+            .fail(function(data) {});
+    } else {
+        clearInterval(interval);
+    }, 5000);
+
     function buildHTML(message) {
         var img = ""
         if (message.image) {
