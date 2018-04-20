896de95c4a1827b0340c7519d49bb4ba5a4b841a
Awtanabe <panser098@gmail.com>
2018-04-20 17:44:58 +0900

    データの切り分け respond_to

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -5,6 +5,12 @@ class MessagesController < ApplicationController
   def index
     @message = Message.new
     @messages = @group.messages.includes(:user)
+
+    respond_to do |format|
+      format.html
+      format.json
+    end
+
   end
 
   def create
