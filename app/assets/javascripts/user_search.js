b83336fba28de1f4fe8badc19042c2fab287f772
Awtanabe <panser098@gmail.com>
2018-04-20 15:36:24 +0900

    二重送信の修正

--- app/assets/javascripts/user_search.js
+++ app/assets/javascripts/user_search.js
@@ -1,43 +1,60 @@
-$(function(){
+$(function() {
 
-  var search_list = $('#user-search-result');
-  // var groupUserList = $('#chat-group-users');
+    var search_list = $('#user-search-result');
+    // var groupUserList = $('#chat-group-users');
 
- function appendUser(user) {
-     var html =
-     `<div class="chat-group-user clearfix">
+    function appendUser(user) {
+        var html =
+            `<div class="chat-group-user clearfix">
       <p class="chat-group-user__name">${user.name}</p>
         <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}">追加</a>
         </div>`
-         search_list.append(html);
-       }
-
-
- $("#user-search-field").on("keyup",function(e){
-  e.preventDefault();
-  var input = $("#user-search-field").val();
-
-
-  $.ajax({
-    type: 'GET',
-    url: '/users',
-    data: { keyword: input },
-    dataType: 'json'
-  })
-  .done(function(users){
-    $('#user-search-result').empty();
-  if (users.length !== 0)
-    {  users.forEach(function(user){
-     appendUser(user);
-        });
+        search_list.append(html);
     }
-    else{
-      console.log('test');
-      appendNoUser('一致するユーザーはいません');
-    }
-  })
-    .fail(function(){
-      alert('ユーザー検索に失敗しました')
+
+
+
+    $("#user-search-field").on("keyup", function(e) {
+        e.preventDefault();
+        var input = $("#user-search-field").val();
+        $.ajax({
+                type: 'GET',
+                url: '/users',
+                data: { keyword: input },
+                dataType: 'json'
+            })
+            .done(function(users) {
+                $('#user-search-result').empty();
+                if (users.length !== 0) {
+                    users.forEach(function(user) {
+                        appendUser(user);
+                    });
+                }
+            })
+            .fail(function() {
+                alert('ユーザー検索に失敗しました')
+            })
     })
-});
+
+    function addUser(user_id, user_name) {
+        var html =
+            `<div class='chat-group-user learfix js-chat-member' id='chat-group-user-8'>
+      <input name='group[user_ids][]' type='hidden' value='${user_id}'>
+        <p class='chat-group-user__name'>${user_name}</p>
+        <a class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</a>
+    </div>`
+        $('#chat-group-users').append(html);
+    }
+
+    $('#user-search-result').on('click', '.chat-group-user__btn--add', function() {
+        var user_id = $(this).data('user-id');
+        var user_name = $(this).data('user-name');
+        addUser(user_id, user_name);
+        $(this).parent().remove();
+    })
+
+    $('#user-search-result').on('click', '.chat-group-user__btn--remove', function() {
+        $(this).parent().remove();
+    })
+
 });
