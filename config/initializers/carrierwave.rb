ec42ddada98cab7c051088d73ec42095c38aa780
Awatanabe <panser098@gmail.com>
2019-04-16 11:08:58 +0900

    s3 設定を解除

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,15 +3,15 @@ require 'carrierwave/storage/file'
 require 'carrierwave/storage/fog'
 
 CarrierWave.configure do |config|
-  config.storage = :fog
-  config.fog_provider = 'fog/aws'
-  config.fog_credentials = {
-    provider: 'AWS',
-    aws_access_key_id: ENV["AWS_ACCESS_KEY_ID"],
-    aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
-    region: 'ap-northeast-1'
-  }
+  config.storage = :file
+  # config.fog_provider = 'fog/aws'
+  # config.fog_credentials = {
+  #   provider: 'AWS',
+  #   aws_access_key_id: ENV["AWS_ACCESS_KEY_ID"],
+  #   aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
+  #   region: 'ap-northeast-1'
+  # }
 
-  config.fog_directory  = 'image-chat-space'
-  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/image-chat-space'
+  # config.fog_directory  = 'image-chat-space'
+  # config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/image-chat-space'
 end
