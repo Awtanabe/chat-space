c004b174f84db5a9b46c9da488f5fbaeea5ddeac
Awatanabe <panser098@gmail.com>
2019-04-30 09:39:04 +0900

    unicron最初の設定

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -10,16 +10,15 @@
 # 8.例外処理 rescue
 # 9.worker_processesとはなんだ？プロセスの理解
 
-app_path = File.expand_path('../../../', __FILE__)
+app_path = File.expand_path('../../', __FILE__)
 
 worker_processes 1
 
-working_directory "#{app_path}/current"
-listen "#{app_path}/shared/tmp/sockets/unicorn.sock"
-pid "#{app_path}/shared/tmp/pids/unicorn.pid"
-stderr_path "#{app_path}/shared/log/unicorn.stderr.log"
-stdout_path "#{app_path}/shared/log/unicorn.stdout.log"
-
+working_directory app_path
+pid "#{app_path}/tmp/pids/unicorn.pid"
+listen "#{app_path}/tmp/sockets/unicorn.sock"
+stderr_path "#{app_path}/log/unicorn.stderr.log"
+stdout_path "#{app_path}/log/unicorn.stdout.log"
 
 listen 3000
 timeout 60
